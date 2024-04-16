bb83101b0a5ee6a568d1654f94e5c144c9a41144
AbhishekNanda7429 <v-abhishek.nanda@kfintech.com>
2024-04-16 11:09:19 +0530

    updated home page

--- front-end/src/Components/Home.js
+++ front-end/src/Components/Home.js
@@ -1,19 +1,27 @@
 import React from "react";
+import myIcon from './svg/myIcon.svg';
 
 function Home()
 {
-    return(
-        <React.Fragment>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                    <h5 className="mt-2">CRM Demo Simulation V1</h5>
-                    </div>
-                </div>
-            </div>
+    // return(
+    //     <React.Fragment>
+    //         <div className="container">
+    //             <div className="row">
+    //                 <div className="col-md-12">
+    //                 <h5 className="mt-2">CRM Demo Simulation V1</h5>
+    //                 <mySVG />
+    //                 </div>
+    //             </div>
+    //         </div>
             
-        </React.Fragment>
-    );
+    //     </React.Fragment>
+    // );
+    return (
+        <div className="home">
+          <h1>CRM Demo Simulation V1</h1>
+          <img src={myIcon} alt="My SVG Image"/>
+        </div>
+      );
 }
 
 export default Home;
